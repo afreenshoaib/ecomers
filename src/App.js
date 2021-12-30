dce9c539707f45549c0a3b2bea710da55236b74d
Afreen <afreenshoaib15@gmail.com>
2021-12-30 16:15:46 +0400

    success

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import {Homepage} from './component/Homepage';
+import './component/homepage.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+
+         </div>
+  );
+}
+
+export default App;
